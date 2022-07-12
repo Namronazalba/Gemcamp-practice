17bd5e1e603647e5809d157a6373ef3106590e8b
Norman.Ablaza <namronazalba@gmail.com>
2022-07-12 14:48:00 +0800

    add crud funtions

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,59 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  before_action :set_post, only: :show
+  before_action :set_own_post, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.includes(:user, :categories)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def show; end
+
+  def destroy
+    if @post.destroy
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def set_own_post
+    @post = current_user.posts.find_by_id(params[:id])
+    if @post.nil?
+      flash[:alert] = 'this post not belongs_to you or not exists'
+      redirect_to posts_path
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :image, category_ids: [])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
