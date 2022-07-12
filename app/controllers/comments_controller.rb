d9c0f6c722a9bf81218e9fa9de5173f3375d7f32
Norman.Ablaza <namronazalba@gmail.com>
2022-07-12 14:47:29 +0800

    add crud funtions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,55 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
+  def index
+    @comments = @post.comments
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render :new
+    end
+  end
+  def show
+
+  end
+
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to post_comments_path(@post, @comment)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @comment.destroy
+      redirect_to post_comments_path(@post)
+    end
+  end
+
+  private
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def set_post
+    @post = Post.find params[:post_id]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ApplicationRecord
+  validates_presence_of :title
+  validates_presence_of :content
+
+  has_many :comments
+  belongs_to :user
+  has_many :post_categoryships
+  has_many :categories, through: :post_categoryships
+
+  # mount_uploader :image, ImageUploader
+end
