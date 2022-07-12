17ee3fb9b40d364d1db1314f51542d83dae665fe
Norman.Ablaza <namronazalba@gmail.com>
2022-07-12 15:26:29 +0800

    install bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,11 @@
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
-.....
-import "bootstrap"
-....
-
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import 'bootstrap'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
