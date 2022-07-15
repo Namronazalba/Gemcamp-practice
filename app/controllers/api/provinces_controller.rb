9c90bbd91dabdb4ab0f3cb5836f28a693f86913e
Norman.Ablaza <namronazalba@gmail.com>
2022-07-15 16:36:42 +0800

    province api

---
+++ app/controllers/api/provinces_controller.rb
@@ -0,0 +1,8 @@
+class Api::ProvincesController < ApplicationController
+  def index
+    @provinces = Province.all
+
+    render json: @provinces, each_serializer: ProvinceSerializer
+  end
+
+end
---
+++ app/serializers/province_serializer.rb
@@ -0,0 +1,8 @@
+class ProvinceSerializer < ActiveModel::Serializer
+  attributes :code, :name, :region
+
+  def region
+    RegionSerializer.new(object.region)
+  end
+
+end
